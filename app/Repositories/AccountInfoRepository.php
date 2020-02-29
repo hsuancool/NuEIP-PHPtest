2c5944e78ad7ed2f60bd9d7fc2159bea345c38a0
Hsuan Ho <hsuan.ho@gcourage.com>
2020-03-01 00:17:51 +0800

    Feature: create AccountInfoRepository getValid method

--- app/Repositories/AccountInfoRepository.php
+++ app/Repositories/AccountInfoRepository.php
@@ -2,7 +2,21 @@
 
 namespace App\Repositories;
 
+use App\Models\AccountInfo;
+
 class AccountInfoRepository implements AccountInfoRepositoryInterface
 {
+    protected $account_info;
 
+    public function __construct(AccountInfo $account_info)
+    {
+        $this->account_info = $account_info;
+    }
+
+    public function getValid()
+    {
+        return $this->account_info
+            ->whereNull('deleted_at')
+            ->get();
+    }
 }
