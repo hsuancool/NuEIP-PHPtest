a7b6c5eb9af1f54e53522fd2a03bc3682e4abd75
Hsuan Ho <hsuan.ho@gcourage.com>
2020-03-01 00:37:19 +0800

    Fix: controller not found error

--- app/Http/Controllers/User/AccountInfoController.php
+++ app/Http/Controllers/User/AccountInfoController.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\User;
 
+use App\Http\Controllers\Controller;
 use App\Services\AccountInfoService;
 use Illuminate\Http\Request;
 
