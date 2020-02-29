97c4cf933f7baa40ff37dc2254ceb8fd2b27308a
Hsuan Ho <hsuan.ho@gcourage.com>
2020-03-01 00:36:37 +0800

    Feature: create Controller response related methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Helper\ResponseFormatHelper;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Response;
 use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    protected function responseSuccessJsonWithFormat(array $data)
+    {
+        return ResponseFormatHelper::responseSuccessJson($data, Response::HTTP_OK);
+    }
+
+    protected function responseFailedJsonWithFormat($status, $message)
+    {
+        return ResponseFormatHelper::responseFailedJson($status, $message);
+    }
 }
