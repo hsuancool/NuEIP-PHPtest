6981b96ca50e72193893127e58cb0aa37b6f6c20
Hsuan Ho <hsuan.ho@gcourage.com>
2020-02-29 18:22:12 +0800

    Feature: create AccountInfo model

---
+++ app/AccountInfo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccountInfo extends Model
+{
+    protected $primaryKey = 'id';
+    protected $table = 'account_info';
+}
