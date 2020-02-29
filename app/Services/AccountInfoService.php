37e4dacdb5a7d25e5935f0241651803d69d363f6
Hsuan Ho <hsuan.ho@gcourage.com>
2020-02-29 18:27:29 +0800

    Feature: create AccountInfoService

---
+++ app/Services/AccountInfoService.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Services;
+
+use App\Repositories\AccountInfoRepository;
+
+class AccountInfoService
+{
+    protected $account_info_repo;
+
+    public function __construct(AccountInfoRepository $account_info_repo)
+    {
+        $this->account_info_repo = $account_info_repo;
+    }
+}
