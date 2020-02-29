f46e9a3c2fb006497680f5e50b1fe342c7292894
Hsuan Ho <hsuan.ho@gcourage.com>
2020-02-29 18:21:52 +0800

    Migration: create create account info migration

---
+++ database/migrations/2020_02_29_051034_create_account_info_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountInfoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('account_info', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('account', 50)->unique();
+            $table->string('name', 255);
+            $table->string('gender');
+            $table->date('birth');
+            $table->string('email', 255)->unique();
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('account_info');
+    }
+}
