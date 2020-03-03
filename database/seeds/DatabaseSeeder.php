e37421b9471a67f5ae6783a43d7dcebb74cc265e
Hsuan Ho <hsuan.ho@gcourage.com>
2020-03-04 04:09:42 +0800

    Feature: add account info seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        factory(\App\Models\AccountInfo::class, 40)->create();
     }
 }
