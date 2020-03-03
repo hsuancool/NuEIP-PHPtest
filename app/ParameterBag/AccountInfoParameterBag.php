07e96f5a034209858ecdaa0929d7514dcc84c29c
Hsuan Ho <hsuan.ho@gcourage.com>
2020-03-03 13:09:05 +0800

    Feature: update AccountInfoParameterBag for create and update

--- app/ParameterBag/AccountInfoParameterBag.php
+++ app/ParameterBag/AccountInfoParameterBag.php
@@ -32,11 +32,42 @@ class AccountInfoParameterBag extends BaseParameterBag
     {
     }
 
+    protected function rulesForCreate()
+    {
+        return [
+            AccountInfoParam::ACCOUNT => 'required|regex:/[a-zA-Z0-9]/i',
+            AccountInfoParam::NAME => 'required',
+            AccountInfoParam::GENDER => 'required|in:0,1',
+            AccountInfoParam::BIRTH => 'required|date',
+            AccountInfoParam::EMAIL => 'required|email',
+        ];
+    }
+
+    protected function rulesForUpdate()
+    {
+        return [
+            AccountInfoParam::NAME => 'string',
+            AccountInfoParam::GENDER => 'in:0,1',
+            AccountInfoParam::BIRTH => 'date',
+            AccountInfoParam::EMAIL => 'email',
+        ];
+    }
+
     protected function rulesForBatchDelete()
     {
         return [
             AccountInfoParam::ACCOUNT_IDS => 'required|array'
         ];
+    }
+
+    public function validateCreate()
+    {
+        $this->validate($this->rulesForCreate());
+    }
+
+    public function validateUpdate()
+    {
+        $this->validate($this->rulesForUpdate());
     }
 
     public function validateBatchDelete()
